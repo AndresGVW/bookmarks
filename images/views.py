695d707ed10635ee002cd25e094818259a175bf1
AndresGVW <andresvonwalter@gmail.com>
2017-09-26 21:23:10 +0100

    Adding Ajax Actions with jQuery

--- images/views.py
+++ images/views.py
@@ -5,9 +5,9 @@ from django.contrib import messages
 from .models import Image
 
 
-# from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse
 # from django.views.decorators.http import require_GET
-# from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST
 # from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # from common.decorators import ajax_required
 
@@ -42,3 +42,21 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html', {'section': 'images',
                                                         'image': image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+    return JsonResponse({'status': 'ko'})
